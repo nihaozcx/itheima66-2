920f09e202418e0125737c9a3865ff4c4b4a2f7b
123 <123@itcast.cn>
2018-11-09 11:39:40 +0800

    125665

--- git_demo/src/main/java/cn/itcast/domain/Customer.java
+++ git_demo/src/main/java/cn/itcast/domain/Customer.java
@@ -3,5 +3,6 @@ package cn.itcast.domain;
 public class Customer {
     public static void main(String[] args) {
         System.out.println("nihaozcx");
+		System.out.println("nihaozhang")
     }
 }
